9ad1d4a1b938b384c6c3997c6396c5ce12a69634
jaficial <jedaficial1@gmail.com>
2024-09-29 12:14:40 -0700

    began writing the movie class

--- movie.cpp
+++ movie.cpp
@@ -13,3 +13,24 @@ using namespace std;
  *
  * input has to come from the provided input file
  * */
+
+class Movie {
+pritvate:
+	string movie_title;
+	int year_released;
+	string screen_writer;
+
+public:
+	string get_movie_title() {return movie_title;}
+	void set_title(string title) {movie_title = title;}
+	
+	int get_year_released() {return year_released;}
+	void set_year(int year) {year_released = year;}
+
+	string screen_writer
+
+};
+
+
+
+
